ff139f0816f732bd4a9311cde2d6047b0aa92c2d
Anuj Mathur <anuj.mathur@rewardle.com>
2021-03-17 11:55:21 +0000

    Removed transition instead of used CSStransition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { Transition } from 'react-transition-group';
+import React from "react";
+import CSSTransition  from "react-transition-group/CSSTransition";
 
-import './Modal.css';
-const animationTiming={
-    enter:400,
-    exit:1000,
-}
+import "./Modal.css";
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
 const modal = (props) => {
-    return(
-        <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
-            {state=>{
-                 let modalToggle = ['Modal',state === 'entering' ?'ModalOpen':state === 'exiting'? 'ModalClose':null]
-                return(
-                    <div className={modalToggle.join(' ')}>
-                    <h1>A Modal</h1>
-                    <button className="Button" onClick={props.closed}>Dismiss</button>
-                </div>
-                )
-            }}
-        </Transition>
-   
-    )
-
+  return (
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames="fade-slide"
+    >
+      <div className='Modal'>
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
 };
 
 export default modal;
